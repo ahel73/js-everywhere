7beaab499635ef16bbb0b9f549b19acec9678b90
Нагаев Алексей <nag495@yandex.ru>
2022-02-25 10:21:18 +0300

    создание сервера

--- api/src/index.js
+++ api/src/index.js
@@ -1,2 +1,33 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+// Схема с помощью языка схем GraphQl
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Распознователь полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!!!!'
+  }
+};
+
+const app = express();
+
+// Настраиваем Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port} - ${server.graphqlPath}`
+  )
+);
