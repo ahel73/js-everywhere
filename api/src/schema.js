a11092b58c0e1b49d2f44575cca33c6137f20164
Нагаев Алексей <nag495@yandex.ru>
2022-03-11 11:08:27 +0300

    6 создание CRUD запросов

--- api/src/schema.js
+++ api/src/schema.js
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server-express');
 
 // Схема с помощью языка схем GraphQl
 module.exports = gql`
+  scalar DateTime
   type Query {
     hello: String!
     notes: [Note]!
@@ -11,8 +12,12 @@ module.exports = gql`
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
   type Mutation {
     newNote(content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
+    deleteNote(id: ID!): Boolean!
   }
 `;
